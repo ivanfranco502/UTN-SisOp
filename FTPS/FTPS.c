d5ef3da6521314b81edf0365a14ad9ebc525679e
1370145.Abad.IgnacioJavier <51642963+ignaciojabad@users.noreply.github.com>
2011-05-02 20:08:45 +0000

    Se agregó el cierre de handles y sockets cuando el thread de control termina. Se sacó el malloc y se uso HeapAlloc.

--- FTPS/FTPS.c
+++ FTPS/FTPS.c
@@ -5,35 +5,46 @@
 #include <stdio.h>
 #include <process.h>   /* _beginthreadex, _endthreadex */
 
+#define CANTIDAD_CLIENTES 65535
+
 WSADATA wsaData;
+int threadsFinalizados[CANTIDAD_CLIENTES]; /*Empiezan en cero y se ponen a uno cuando finaliza el thread */
+
+
+void inicializarVectorDeThreads(){
+	// Inicializa en cero threadsFinalizados[]
+	int i=0;
+	for (i; i<CANTIDAD_CLIENTES; i++)
+		threadsFinalizados[i]=0;
+}
 
 
 /*unsigned*/ __stdcall threadClienteNuevo( void* pArguments ){
-    int *remoteClient;
+    // Thread de datos
+	int *remoteClient;
 	remoteClient = (int*) pArguments;
 	
-	/* aca va el send y rcv de datos*/
-
+	/* aca va el send y rcv de datos */
+	
+	//printf("Thread del cliente");
 	
 	
 	
-
+	threadsFinalizados[*remoteClient]=1;
     _endthreadex( 0 );
-    return *remoteClient;
+    
 } 
 
 int main(){ 
-    HANDLE hThread[1000];
-    unsigned threadID[1000];
+    HANDLE hThread[CANTIDAD_CLIENTES];
+    unsigned threadID[CANTIDAD_CLIENTES];
 	SOCKET descriptor;
 	int *remoteClient;
-	int numeroDeCliente =0;
-	int a, addrlen = sizeof(struct sockaddr_in);
-	struct sockaddr_in *local_address= malloc(addrlen);
-    struct sockaddr_in *remote_address= malloc(addrlen);
-
+	int i, a, addrlen = sizeof(struct sockaddr_in);
+	struct sockaddr_in *local_address = HeapAlloc(GetProcessHeap(), 0, addrlen);
+    struct sockaddr_in *remote_address = HeapAlloc(GetProcessHeap(), 0, addrlen);
 	
-	remoteClient = (int *) malloc(sizeof(int));
+	remoteClient = (int *) HeapAlloc(GetProcessHeap(), 0, sizeof(int));
 
 	if ((a = WSAStartup(MAKEWORD(2, 2), &wsaData)) != 0) {
 		printf("WSAStartup() devolvio error nro %d\n", a );
@@ -41,41 +52,37 @@ int main(){
 	}
 	
 	
-
     local_address->sin_family = AF_INET;
 	local_address->sin_addr.s_addr=INADDR_ANY;
 	local_address->sin_port = htons (21);
 
-	descriptor= socket(AF_INET, SOCK_STREAM, 0);
+	printf("%d\n",descriptor= socket(AF_INET, SOCK_STREAM, 0));
 	bind (descriptor,(struct sockaddr *) local_address, addrlen);
 	listen(descriptor,100);
 
     printf("estoy escuchando el puerto de mierda\n");
-
+	
+	inicializarVectorDeThreads();
+	
 	while(1){
 		if((*remoteClient = accept (descriptor, (struct sockaddr *)remote_address, (void*)&addrlen))!= -1){
 			printf("%d", *remoteClient);
-			hThread[*remoteClient] = (HANDLE) _beginthreadex(NULL,0, &threadClienteNuevo, (void*) remoteClient, 0, &threadID[numeroDeCliente]);
-		
-			//CloseHandle(hThread[*remoteClient]);
-		
-			numeroDeCliente++;
+			hThread[*remoteClient] = (HANDLE) _beginthreadex(NULL,0, &threadClienteNuevo, (void*) remoteClient, 0, &threadID[*remoteClient]);
 		}
+
+		for (i=0; i<CANTIDAD_CLIENTES;i++){
+			if (threadsFinalizados[i]){
+				closesocket(i);
+				CloseHandle(hThread[i]);
+			}
+		}
+			
+		
 	}
 
         
 
-	
-	
-	
 
-	
 
-    
-   
 
-	
-	
-	
-	getchar();
 }
