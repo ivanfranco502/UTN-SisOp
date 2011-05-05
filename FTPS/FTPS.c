639449b04da9e2d3bcc682285faf39661169d226
1370145.Abad.IgnacioJavier <51642963+ignaciojabad@users.noreply.github.com>
2011-05-05 23:16:56 +0000

    Servidor FTP semi funcionando, se conecta un cliente y le muestra los archivos y directorios que hay en el el servidor. Hasta ahi todo bien, falta el resto (que se pueda bajar y eso, e interpretar los comandos). Tal vez se pueda depurar mejor, pero por lo menos funciona un toque.

--- FTPS/FTPS.c
+++ FTPS/FTPS.c
@@ -4,8 +4,13 @@
 #include <windows.h>
 #include <stdio.h>
 #include <process.h>   /* _beginthreadex, _endthreadex */
+#include <string.h>
+#include "funcionesftp.h"
+
+
 
 #define CANTIDAD_CLIENTES 65535
+#define SOCKET_MAX_BUFFER 100
 
 WSADATA wsaData;
 int threadsFinalizados[CANTIDAD_CLIENTES]; /*Empiezan en cero y se ponen a uno cuando finaliza el thread */
@@ -19,21 +24,87 @@ void inicializarVectorDeThreads(){
 }
 
 
-unsigned __stdcall threadClienteNuevo( void* pArguments ){
+unsigned __stdcall threadDeDatos( void* pArguments ){
     // Thread de datos
-	
+
+	HANDLE  heapDatos = HeapCreate(HEAP_NO_SERIALIZE, 1024*1024, 0);
+	SOCKET descriptorD;
+	int clienteDatos;
+	int addrlen = sizeof(struct sockaddr_in);
+	struct sockaddr_in *local_address = HeapAlloc(heapDatos, 0, addrlen);
+    struct sockaddr_in *remote_address = HeapAlloc(heapDatos, 0, addrlen);
+	char buffer[SOCKET_MAX_BUFFER];
+	int *remoteClient  = HeapAlloc(heapDatos, HEAP_NO_SERIALIZE, sizeof(int));
+	remoteClient = (int*) pArguments;
+
+
+    local_address->sin_family = AF_INET;
+	local_address->sin_addr.s_addr=INADDR_ANY;
+	local_address->sin_port = htons (5300);
+
+	//printf("%d\n",descriptor= socket(AF_INET, SOCK_STREAM, 0));
+	printf("%d\n",descriptorD= socket(AF_INET, SOCK_STREAM, 0));
+	bind (descriptorD,(struct sockaddr *) local_address, addrlen);
+	listen(descriptorD,100);
+
+	enviar_comando(*remoteClient,"227 Entering Pasive Mode (192,168,140,129,20,180)\r\n",buffer);
+
+	printf("%d\n", clienteDatos = accept(descriptorD, (struct sockaddr *)remote_address, (void*)&addrlen));
+
+	if(clienteDatos!=INVALID_SOCKET){
+		Sleep(10);
+		enviar_comando_no_recv(clienteDatos,"150 Openning ASCII mode\r\n",buffer);
+		Sleep(10);
+		send(clienteDatos,"-rwxrwxrwx 1 ftp ftp 4096 Sep 02 2009 archivo1.txt\r\n",strlen("-rwxrwxrwx 1 ftp ftp 4096 Sep 02 2009 archivo1.txt\r\n"),0);
+		send(clienteDatos,"-rwxrwxrwx 1 ftp ftp 4096 Sep 02 2009 archivo2.txt\r\n",strlen("-rwxrwxrwx 1 ftp ftp 4096 Sep 02 2009 archivo2.txt\r\n"),0);
+		send(clienteDatos,"drwxrwxrwx 1 ftp ftp 4096 Sep 02 2009 directorio1\r\n",strlen("drwxrwxrwx 1 ftp ftp 4096 Sep 02 2009 directorio1\r\n"),0);
+		
+		closesocket(clienteDatos);
+		closesocket(descriptorD);
+	}else
+		printf("No se conectaron al thread de datos\n");
+	//HeapFree(heap, HEAP_NO_SERIALIZE, remoteClient);
+	HeapDestroy(heapDatos);
+    _endthreadex( 0 );
+} 
+
+
+
+unsigned __stdcall threadClienteNuevo( void* pArguments ){
+    // Thread de comandos
 	
 	HANDLE  heap = HeapCreate(HEAP_NO_SERIALIZE, 1024*1024, 0);
-
-	
+	HANDLE hThreadDatos;
+	int corrector;
+	char buffer[100];
 	int *remoteClient  = HeapAlloc(heap, HEAP_NO_SERIALIZE, sizeof(int));
 	remoteClient = (int*) pArguments;
 	
+	
 	/* aca va el send y rcv de datos */
+	/*printf("Thread del cliente\n");
+	recv(*remoteClient, buffer, SOCKET_MAX_BUFFER*sizeof(char),0);
+	printf("%s\n",buffer);*/
 	
-	//printf("Thread del cliente");
+	enviar_comando(*remoteClient,"220 POWER\r\n",buffer);
+	enviar_comando(*remoteClient,"230 POWER\r\n",buffer);
+	wait_for(*remoteClient,"PWD\r\n",buffer);
+	enviar_comando(*remoteClient,"257 \"/\" Is the current directory \r\n",buffer);
+	//wait_for(*remoteClient,"TYPE A\r\n",buffer);
+	//enviar_comando(*remoteClient,"250 CWD command successful\r\n",buffer);
+	if (!(strcmp(buffer, "noop")))
+		enviar_comando(*remoteClient,"200 NOOP Command succesfull /\r\n",buffer);
+	if (!(strcmp(buffer, "CWD /")))
+		enviar_comando(*remoteClient,"200 ubicacion /\r\n",buffer);
+	enviar_comando(*remoteClient,"200 Type set to A\r\n",buffer);
+	Sleep(5);
 	
-	
+	hThreadDatos = (HANDLE) _beginthreadex(NULL,0, &threadDeDatos, (void*) remoteClient, 0, NULL);
+	WaitForSingleObject(hThreadDatos, INFINITE);
+	CloseHandle(hThreadDatos);
+
+	enviar_comando(*remoteClient,"226 Transfer Complete\r\n",buffer);
+
 	
 	//HeapFree(heap, HEAP_NO_SERIALIZE, remoteClient);
 	HeapDestroy(heap);
@@ -41,6 +112,8 @@ unsigned __stdcall threadClienteNuevo( void* pArguments ){
     _endthreadex( 0 );
     
 } 
+
+
 
 int main(){ 
     HANDLE hThread[CANTIDAD_CLIENTES];
@@ -73,7 +146,7 @@ int main(){
 	
 	while(1){
 		if((*remoteClient = accept (descriptor, (struct sockaddr *)remote_address, (void*)&addrlen))!= -1){
-			printf("%d", *remoteClient);
+			printf("%d\n", *remoteClient);
 			hThread[*remoteClient] = (HANDLE) _beginthreadex(NULL,0, &threadClienteNuevo, (void*) remoteClient, 0, &threadID[*remoteClient]);
 		}
 
@@ -82,6 +155,7 @@ int main(){
 				closesocket(i);
 				CloseHandle(hThread[i]);
 			}
+
 		}
 			
 		
---
+++ FTPS/funcionesftp.h
@@ -0,0 +1,4 @@
+int enviar_comando(int, char *, char *);
+int enviar_comando_no_recv(int,char*,char*);
+int wait_for (int, char *, char *);
+
