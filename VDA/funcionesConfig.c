d5bdb78fbc49bc2c9fd0dbc47f9aae85d9291aa9
Iván Franco <22622318+ivanfranco502@users.noreply.github.com>
2011-06-16 20:37:10 +0000

    Implementacion final del config para VDA
    Envio modelo de config para configuraciones posteriores. Cambie el formato del config a uno mucho mas sencillo de crear

--- VDA/funcionesConfig.c
+++ VDA/funcionesConfig.c
@@ -196,3 +196,84 @@ void getConfigFTP (char *IP, char *PORT){
 	strcpy(IP, dameIP (buffer));
 	strcpy(PORT, damePuerto (buffer));
 }
+
+int obtenerParametroParaStruct(char *buffer, char *variableParametro, int arrancaContadorBuffer){
+	int contadorBuffer,
+		contadorParametro=0;
+	
+	contadorBuffer = arrancaContadorBuffer;
+
+	while(buffer[contadorBuffer]!= '='){
+		contadorBuffer++;
+	}
+	contadorBuffer++;
+	while((buffer[contadorBuffer]!= '\n') && (buffer[contadorBuffer]!='\0') && (buffer[contadorBuffer]!='\r')){
+		variableParametro[contadorParametro] = buffer[contadorBuffer];
+		contadorParametro++;
+		contadorBuffer++;
+	}
+	variableParametro[contadorParametro]= '\0';
+	return(contadorBuffer);
+}
+
+void obtenerStructVDA(char *buffer, configVDA *configuracionInicial){
+	char *variableParametro[50];
+	int contadorBuffer=0;
+
+	//nombreVDA
+	contadorBuffer = obtenerParametroParaStruct(buffer, variableParametro, contadorBuffer);
+	strcpy(configuracionInicial->nombreVDA, variableParametro);
+	//ipKSS
+	contadorBuffer = obtenerParametroParaStruct(buffer, variableParametro, contadorBuffer);
+	strcpy(configuracionInicial->ipKSS, variableParametro);
+	//puertoKSS
+	contadorBuffer = obtenerParametroParaStruct(buffer, variableParametro, contadorBuffer);
+	sscanf(variableParametro, "%d", &configuracionInicial->puertoKss);
+	//cilindros
+	contadorBuffer = obtenerParametroParaStruct(buffer, variableParametro, contadorBuffer);
+	sscanf(variableParametro, "%d", &configuracionInicial->cilindros);
+	//headers
+	contadorBuffer = obtenerParametroParaStruct(buffer, variableParametro, contadorBuffer);
+	sscanf(variableParametro, "%d", &configuracionInicial->headers);
+	//sectores
+	contadorBuffer = obtenerParametroParaStruct(buffer, variableParametro, contadorBuffer);
+	sscanf(variableParametro, "%d", &configuracionInicial->sectores);
+	//retardoRotacional
+	contadorBuffer = obtenerParametroParaStruct(buffer, variableParametro, contadorBuffer);
+	sscanf(variableParametro, "%d", &configuracionInicial->retardoRotacional);
+	//posicionCabezal
+	contadorBuffer = obtenerParametroParaStruct(buffer, variableParametro, contadorBuffer);
+	sscanf(variableParametro, "%d", &configuracionInicial->posicionCabezal);
+	//cacheActivada
+	contadorBuffer = obtenerParametroParaStruct(buffer, variableParametro, contadorBuffer);
+	sscanf(variableParametro, "%d", &configuracionInicial->cacheActivada);
+}
+
+void getConfigVDA(configVDA *config){
+	HANDLE *archivo;
+	DWORD bytesLeidos;
+	LPCSTR nombreArchivo = "vda.config";
+	char buffer[300];
+	int lectura;
+	char variableAuxiliarConfig[100]; //variable que me sirve para obtener los parametros
+
+	//apertura archivo
+	archivo = CreateFileA (nombreArchivo, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	if (archivo == INVALID_HANDLE_VALUE){
+		return;
+	}
+
+	//lectura archivo
+	lectura = ReadFile (archivo, buffer, sizeof(buffer), &bytesLeidos, NULL);
+
+	buffer[bytesLeidos] = '\0';
+
+	if (lectura == 0) {
+		return;
+	}
+
+	CloseHandle(archivo);
+
+	obtenerStructVDA(buffer, config);//funcion que se le pasa el buffer y el structVDA
+}
--- VDA/funcionesConfig.h
+++ VDA/funcionesConfig.h
@@ -1,3 +1,25 @@
+typedef struct{
+	char nombreVDA[50];
+	char ipKSS[16];
+	unsigned puertoKss[6];
+	int cilindros;
+	int headers;
+	int sectores;
+	int retardoRotacional;
+	int posicionCabezal;
+	int cacheActivada; 
+}configVDA;
+
+typedef struct{
+	char ipConexion[16];
+	unsigned puertoConexion[6];
+	char path[100];
+	char ipKSS[16];
+	unsigned puertoKss[6]; 
+}configFTP;
+
+void obtenerStructVDA(char *, configVda *configuracionInicial);
+int obtenerParametroParaStruct(char *buffer, char *variableParametro, int arrancaContadorBuffer);
 char *damePuerto(char *); 
 char *dameIP (char *);
 char *getPath (char *);
@@ -6,3 +28,4 @@ void getConfigPath (char *);
 char *getIPKSS(char *Buff);
 char *getPortKSS(char *Buff);
 void getConfigKSS (char *IP, unsigned puerto);
+void getConfigVDA(configVDA *config);
