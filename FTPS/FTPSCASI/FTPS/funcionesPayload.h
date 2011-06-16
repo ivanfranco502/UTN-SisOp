15f7f7142d7ba4569ae389ffc4fe1eccf388546d
1370935.Franco.Ivan <1370935.Franco.Ivan@3bac5ed1-cde4-4838-9d27-a975ff9aede3>
2011-06-16 23:27:02 +0000

    Subo el Struct del MPS_Package con PACK

--- FTPS/FTPSCASI/FTPS/funcionesPayload.h
+++ FTPS/FTPSCASI/FTPS/funcionesPayload.h
@@ -1,9 +1,10 @@
-typedef struct{
-	char DescriptorID[15];   //único, si usamos la hora + un random es suficiente.
-	char PayloadDescriptor;  //tipo de operacion (handshake, vda, fss, ftp, list, cd, etc)
-	int PayloadLenght;  //longitud del payload, 0 si no enviamos nada.
-	char Payload[1050];  //mensaje.
-}MPS_Package;
+#define PACK( __Declaration__ ) __pragma( pack(push, 1) ) __Declaration__ __pragma( pack(pop) )
 
+PACK(typedef struct{
+        char DescriptorID[16];   //unico, si usamos la hora + un random es suficiente.
+        char PayloadDescriptor;  //tipo de operacion (handshake, vda, fss, ftp, list, cd, etc)
+        int PayloadLenght;  //longitud del payload, 0 si no enviamos nada.
+        char Payload[1050];  //mensaje.
+}MPS_Package);
 /*void generar_DescriptorID(char *);
 void llenar_Payload(MPS_Package*, char*, char, int, char*);*/
