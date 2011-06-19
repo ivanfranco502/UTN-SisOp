0064d76600c4af761ba1150e41f82890ee48692b
1370935.Franco.Ivan <1370935.Franco.Ivan@3bac5ed1-cde4-4838-9d27-a975ff9aede3>
2011-06-19 18:51:08 +0000

    Se agregan logs basicos y validacion estado del socketKSS

--- VDA/funcionesConfig.h
+++ VDA/funcionesConfig.h
@@ -12,12 +12,4 @@ typedef struct  {
 
 void obtenerStructVDA(char *, configVDA *configuracionInicial);
 int obtenerParametroParaStruct(char *buffer, char *variableParametro, int arrancaContadorBuffer);
-char *damePuerto(char *); 
-char *dameIP (char *);
-char *getPath (char *);
-void getConfigFTP (char *, char *);
-void getConfigPath (char *);
-char *getIPKSS(char *Buff);
-char *getPortKSS(char *Buff);
-void getConfigKSS (char *IP, unsigned puerto);
 void getConfigVDA(configVDA *config);
